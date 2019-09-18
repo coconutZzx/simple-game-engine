8f890b2f519bcba67dbae73cdddfb3bc49e6a3de
coconutZzx <naylorsydney@gmail.com>
2019-09-18 08:46:19 +0200

    Update SnakeSegment.cs

--- SimpleGameEngine/SimpleGameEngine/SnakeSegment.cs
+++ SimpleGameEngine/SimpleGameEngine/SnakeSegment.cs
@@ -6,48 +6,78 @@ public class SnakeSegment{
 
     private char character; //the character representing this snake segment visually
 
-    public SnakeSegment(int x, int y, char character){
+    public SnakeSegment(int x, int y, char character)
+    {
         /*  ------------------------------------------
             1.1
             ------------------------------------------   
             Initialize the x, y and character fields 
             using the values passed to the constructor
         */
+        this.x = x;
+        this.y = y;
+        this.character = character;
     }
 
-    public int X {
-        get {return x;}
-        set {
+    public int X
+    {
+        get
+        {
+            return x;
+        }
+        set
+        {
             /*  ------------------------------------------
                 1.2
                 ------------------------------------------
                 Before overwriting x with the new value,
                 set prevX to the current value of x
             */
+            prevX = x;
+            x = value;
         }
     }
 
-    public int Y {
-        get {return y;}
-        set {
+    public int Y
+    {
+        get
+        {
+            return y;
+        }
+        set
+        {
             /*  ------------------------------------------
                 1.3
                 ------------------------------------------
                 Before overwriting y with the new value,
                 set prevY to the current value of y
             */
+            prevY = y;
+            y = value;
         }
     }
 
-    public int PrevX {
-        get {return prevX;}
+    public int PrevX
+    {
+        get
+        {
+            return prevX;
+        }
     }
 
-    public int PrevY {
-        get {return prevY;}
+    public int PrevY
+    {
+        get
+        {
+            return prevY;
+        }
     }
 
-    public char Character{
-        get {return character;}
+    public char Character
+    {
+        get
+        {
+            return character;
+        }
     }
 }
